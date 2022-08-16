02a594d522d3866cd4ace9902cd70c185cc1bc0e
fuka-g <fuka_main@protonmail.com>
2022-08-16 14:47:36 +0200

    add colors.ts and levels.ts

---
+++ src/utils/colors.ts
@@ -0,0 +1,24 @@
+/**
+ * Exports ANSI excape codes for formatting terminal output.
+ * I use this instead of colors because it's faster and just as easy to use.
+ */
+export default {
+	// Colors
+	gray: "\x1b[90m",
+	red: "\x1b[91m",
+	green: "\x1b[92m",
+	yellow: "\x1b[93m",
+	blue: "\x1b[94m",
+	magenta: "\x1b[95m",
+	cyan: "\x1b[96m",
+	white: "\x1b[97m",
+
+	// Formatting
+	bold: "\x1b[1m",
+	italic: "\x1b[3m",
+	underline: "\x1b[4m",
+	strikethrough: "\x1b[1m",
+
+	// Reset
+	reset: "\x1b[0m"
+}
---
+++ src/utils/levels.ts
@@ -0,0 +1,12 @@
+/**
+ * Numerical values for the levels
+ */
+
+export default {
+	trace: 0,
+	debug: 1,
+	info: 2,
+	warn: 3,
+	error: 4,
+	fatal: 5,
+};
